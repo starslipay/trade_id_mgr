4b3495d80b6d0072e55ea99068be2f9a0d1e2dce
starsli <starsli@tencent.com>
2026-07-18 15:50:11 +0800

    save

---
+++ dev_deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+MODULE_NAME="trade_id_mgr"
+VERSION="v1.0.0"
+IMAGE_NAME="${MODULE_NAME}:${VERSION}"
+
+docker rm -f $MODULE_NAME
+docker rmi -f $IMAGE_NAME
+docker build -t $IMAGE_NAME .
+docker run -d --name $MODULE_NAME --network dev_pay_net -p 30884:8080 $IMAGE_NAME
+# docker run -d --name trade_id_mgr --network dev_pay_net -p 30884:8080 trade_id_mgr:v1.0.0
+docker ps
+docker logs $MODULE_NAME
